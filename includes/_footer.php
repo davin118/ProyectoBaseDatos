744a46380934ec6fbf4ad2102e04781e300142b6
Davin Ortez <davin.ortez118@gmail.com>
2023-06-27 20:28:39 -0600

    Primera version

---
+++ includes/_footer.php
@@ -0,0 +1,43 @@
+<footer class="text-center text-lg-start bg-dark text-muted">
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4  border-bottom">
+
+    <div class="me-3 d-none d-lg-block">
+      <span>Siguenos en nuestras redes sociales:</span>
+    </div>
+       
+        
+    </div>
+  </section>
+    <div class="container text-center text-md-start mt- bg-dark">
+      <div class="row mt-3">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>!Conocenos¡
+          </h6>
+          <p>
+            Esta compañia esta encargada en ayudar a las personas etc...
+          </p>
+        </div>
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contactanos en:
+          </h6>
+          <p><i class="fas fa-home me-3"></i> León, Edificio Ciencias y Tecnologias</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            Información@gmail.com
+          </p>
+          
+        </div>
+      </div>
+    </div>
+    
+  <div class="text-center bd-dark">
+    © 2023 Copyright: Todos los Derechos Reservados 
+  </div>
+  </section>
+
+
+</footer>
+  </body>
